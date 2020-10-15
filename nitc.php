bca4dc69490e674d69c3af964b5f52fe6b3c5fcf
Rahul Dev <rahuldev.w@gmail.com>
2020-10-15 11:50:02 +0530

    nitc page added

---
+++ nitc.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Innovatiview</title> 
+   <link rel="shortcut icon" href="images/fav.jpg" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="newcss/style.css">
+    <link rel="stylesheet" type="text/css" href="css/responsive.css">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/slick-theme.css">
+    <link rel="stylesheet" type="text/css" href="css/slick.min.css">
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+    <link rel="stylesheet" type="text/css" href="css/navigation.css">
+    <link rel="stylesheet" type="text/css" href="css/btn.css">
+    <link rel="stylesheet" type="text/css" href="css/about.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    
+</head>
+
+
+<style>
+    
+   
+</style>
+<body class="bg">
+    <?php $page ='about'; ?>
+<?php include 'navigation.php';?>
+
+<section class="secion-wrappper banner-section mh-100vh d-flex align-items-center justify-content-center">
+  <div class="container-fluid">
+  <div class="banner-inner text-center"> 
+    <div class="sitc-logo mb-4">
+      <img src="./images/nitc/sit-logo.png" width="300" alt="">
+    </div>
+    <p class="text-darkblue">
+      <span>SUPPLY.</span> 
+      <span>INSTALLATiON.</span> 
+      <span>TESTiNG.</span> 
+      <span>COMMiSSiONiNG.</span>
+    </p>
+    </div>
+  </div>
+</section>
+
+
+<section class="secion-wrappper section1 mh-100vh d-flex align-items-center justify-content-center">
+
+  <div class="container">
+
+    <div class="centered-block mx-auto text-center">
+      <div class="pb-4">
+      <h4 class="proxima font-weight-bold text-black  mb-4">About 34% of Indians live in the cities. By 2030, this is expected to soar up to 40.76%.</h4>
+      <p class="proxima font-weight-normal text-black f-20">
+      Urbanization in India is growing aggressively on account of growth opportunities and better living conditions being offered by the cities. As per World Health Organization, India will be amongst the top 5 countries to lead the world's urban population surge by 2050. 
+      </p>
+      <p class="proxima font-weight-normal text-black f-20">The rising urbanization is challenging our existing security and surveillance systems as potential threats and criminal offences are on a constant rise. In addition, it is creating an additional burden on our existing urban resources. Together, these have led to an evident need for a robust and intelligent security and surveillance system that can safeguard our cities and manage them more efficiently. These systems would ensure effective use of urban resources, streamlined traffic system, efficient surveillance of public buildings and high-density spaces and more. </p>
+      </div>
+  
+      <a href="#" class="btn btn-dark blue rounded d-inline-flex align-content-center btn-style1">
+          <img src="./images/nitc/know_more.png" width="120" alt="">
+      </a>
+    </div>
+
+  </div>
+
+</section>
+
+
+<section class="secion-wrappper section2 mh-100vh d-flex align-items-center justify-content-center">
+
+  <div class="container">
+    <div class="centered-block">
+    <div class="row align-items-center">
+      <div class="col-md-6">
+      <div class=" ">
+      <div class="pb-4">
+        <div class="i-logo mb-4">
+          <img src="./images/nitc/logo-Blue.png" width="300" alt="">
+        </div>
+      <h4 class="proxima font-weight-bold text-black f-18 mb-4">Building intelligent security, surveillance & information systems </h4>
+      <p class="proxima font-weight-normal text-black f-20">
+      Innovatiview is an end-to-end system integrator providing solutions across diverse business verticals, with a significant focus on building intelligent security, surveillance and information systems. We integrate state-of-art technologies with reliable surveillance platforms to deliver unique solutions. 
+      </p>
+      </div>
+      <a href="#" class="btn btn-dark blue rounded d-inline-flex align-content-center  btn-style1">
+          <img src="./images/nitc/know_more.png" width="120" alt="">
+      </a>
+    </div>
+      </div>
+      <div class="col-md-6 img-right mb-5 mb-md-0 p-0 p-lg-5">
+        <img src="./images/nitc/website-03.png" class="w-100 side-img" alt="">
+      </div>
+  </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="secion-wrappper section3 mh-100vh d-flex align-items-center justify-content-center">
+
+  <div class="container">
+    <div class="centered-block">
+    <div class="row align-items-center">
+    <div class="col-md-6 mb-5 mb-md-0 p-0 p-lg-5 separator-right">
+        <img src="./images/nitc/website-04.png" class="w-100 side-img" alt="">
+      </div>
+      <div class="col-md-6  pl-md-5">
+      <div class=" ">
+      <div class="pb-4">
+       
+     
+      <p class="proxima font-weight-normal text-black f-20">
+      Monitors the city 24*7 through high-tech cameras to deter and prevent illicit activities in sensitive areas, record irrefutable evidence and enable faster response to emergencies.
+      </p>
+      </div>
+      <a href="#" class="btn btn-dark blue rounded d-inline-flex align-content-center btn-style1">
+          <img src="./images/nitc/know_more.png" width="120" alt="">
+      </a>
+    </div>
+      </div>
+     
+  </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="secion-wrappper section4 mh-100vh d-flex align-items-center justify-content-center">
+
+  <div class="container">
+    <div class="centered-block">
+    <div class="row align-items-center">
+    
+      <div class="col-md-6 separator-right  pr-md-5">
+      <div class=" ">
+      <div class="pb-4">
+       
+     
+      <p class="proxima font-weight-normal text-black f-20">
+      Enables a smooth and effective traffic system to streamline vehicular movement, improve road safety and detect policy violations with automation, analytics and integration through a centralized system.
+      </p>
+      </div>
+      <a href="#" class="btn btn-dark blue rounded d-inline-flex align-content-center btn-style1">
+          <img src="./images/nitc/know_more.png" width="120" alt="">
+      </a>
+    </div>
+      </div>
+
+      <div class="col-md-6 img-right mb-5 mb-md-0 p-0 p-lg-5  btn-style1">
+        <img src="./images/nitc/website-05.png" class="w-100 side-img" alt="">
+      </div>
+     
+  </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="secion-wrappper section5 mh-100vh d-flex align-items-center justify-content-center">
+
+  <div class="container">
+    <div class="centered-block">
+    <div class="row align-items-center">
+    <div class="col-md-6 mb-5 mb-md-0 p-0 p-lg-5 separator-right">
+        <img src="./images/nitc/website-06.png" class="w-100 side-img" alt="">
+      </div>
+      <div class="col-md-6 pl-md-5">
+      <div class=" ">
+      <div class="pb-4">
+       
+     
+      <p class="proxima font-weight-normal text-black f-20">
+      Enables a smooth and effective traffic system to streamline vehicular movement, improve road safety and detect policy violations with automation, analytics and integration through a centralized system.
+      </p>
+      </div>
+      <a href="#" class="btn btn-dark blue rounded d-inline-flex align-content-center btn-style1">
+          <img src="./images/nitc/know_more.png" width="120" alt="">
+      </a>
+    </div>
+      </div>
+
+    
+     
+  </div>
+    </div>
+  </div>
+</section>
+
+<section class="secion-wrappper section6 mh-100vh d-flex align-items-center justify-content-center">
+
+  <div class="container">
+    <div class="centered-block">
+    <div class="row align-items-center">
+    
+      <div class="col-md-6 separator-right pr-md-5">
+      <div class=" ">
+      <div class="pb-4">
+       
+     
+      <p class="proxima font-weight-normal text-black f-20">
+      Enables a smooth and effective traffic system to streamline vehicular movement, improve road safety and detect policy violations with automation, analytics and integration through a centralized system.
+      </p>
+      </div>
+      <a href="#" class="btn btn-dark blue rounded d-inline-flex align-content-center btn-style1">
+          <img src="./images/nitc/know_more.png" width="120" alt="">
+      </a>
+    </div>
+      </div>
+
+      <div class="col-md-6 img-right mb-5 mb-md-0 p-0 p-lg-5">
+        <img src="./images/nitc/website-07.png" class="w-100 side-img" alt="">
+      </div>
+     
+  </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="secion-wrappper section7 mh-100vh d-flex align-items-center justify-content-center">
+
+  <div class="container">
+    <div class="centered-block">
+    <div class="row align-items-center">
+    <div class="col-md-6 mb-5 mb-md-0 p-0 p-lg-5 separator-right">
+        <img src="./images/nitc/website-08.png" class="w-100 side-img" alt="">
+      </div>
+      <div class="col-md-6 pl-md-5">
+      <div class=" ">
+      <div class="pb-4">
+       
+     
+      <p class="proxima font-weight-normal text-black f-20">
+      Enables a smooth and effective traffic system to streamline vehicular movement, improve road safety and detect policy violations with automation, analytics and integration through a centralized system.
+      </p>
+      </div>
+      <a href="#" class="btn btn-dark blue rounded d-inline-flex align-content-center btn-style1">
+          <img src="./images/nitc/know_more.png" width="120" alt="">
+      </a>
+    </div>
+      </div>
+
+    
+     
+  </div>
+    </div>
+  </div>
+</section>
+
+<?php include 'footer.php';?>
+
+
+
+</body>
+</html>
