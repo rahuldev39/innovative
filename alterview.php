84bcb54239fab71c901e506c6b4069d5c426ecfd
Rahul Dev <rahuldev.w@gmail.com>
2020-10-16 12:44:04 +0530

    alterview page added

---
+++ alterview.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Innovatiview</title> 
+   <link rel="shortcut icon" href="images/fav.jpg" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="newcss/style.css">
+    <link rel="stylesheet" type="text/css" href="css/responsive.css">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/slick-theme.css">
+    <link rel="stylesheet" type="text/css" href="css/slick.min.css">
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+    <link rel="stylesheet" type="text/css" href="css/navigation.css">
+    <link rel="stylesheet" type="text/css" href="css/btn.css">
+    <link rel="stylesheet" type="text/css" href="css/about.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    
+</head>
+
+
+<style>
+    
+   
+</style>
+<body class="bg">
+    <?php $page ='about'; ?>
+<?php include 'navigation.php';?>
+
+
+
+
+<section class="secion-wrappper py-5 aliterview-banner   mh-100vh d-flex align-items-center">
+
+  <div class="container">
+    <div class="row ">
+    <div class="col-md-6 ml-0">
+    <div class="banner-text title-border-lg border-orange border-left px-3">
+      <h2 class="proxima font-weight-bold text-black  mb-4 text-alter-blue">EXPERIENCES</h2>
+      <h4 class="text-orange proxima font-weight-bold">BEYOND
+      <br>
+       IMAGINATION</h4>
+      <p class="proxima text-gray font-weight-normal  f-20 mb-0">Delivering world-class experiences through cutting-edge technologies and its innovative
+applications.</p>
+    </div>
+    </div>
+    </div>
+    
+
+  </div>
+
+  </section>
+
+<section class="secion-wrappper bg-gray   mh-100vh  d-flex align-items-center justify-content-center">
+
+  <div class="container">
+
+    <div class="row">
+      <div class="col-lg-6">
+        <div class="title-border-sm border-orange border-left pl-3 mb-4">
+          <h5 class="proxima font-weight-bold text-black  mb-2 text-alter-blue">ALTRVIEW: DELIVERING EXPERIENCES</h5>
+          <h3 class="text-orange proxima font-weight-bold">BEYOND IMAGINATION</h3>
+        </div>
+
+        <p class="proxima text-gray font-weight-normal  f-16 mb-3">We deliver world-class experiences through cutting-edge technologies and its innovative applications.</p>
+        <p class="proxima text-gray font-weight-normal  f-16 mb-0">Being part of the prestigious conglomerate Innovatiview, altRview creates exceptional experiences by bringing bleeding-edge technologies, creativity and execution excellence together. Our offerings are crafted to partner with the Government Sectors like Zoos, Museums, Science Cities and Historical Monuments.
+Ourjourney of success started with Robotic V Shera- the robotic version of the mascot of Commonwealth Games 2010. Since then, we have invested across India and have developed some of the most
+spectacular Animatronics Projects, High-tech Interpretation Park,
+ Scientiﬁc Galleries and Museums in the country.        </p>
+      </div>
+      <div class="col-lg-6 img-lg-right mb-5 mb-lg-0">
+        <img src="./images/alterview/av-img1.png" class="img-fluid" alt="">
+      </div>
+    </div>
+
+  </div>
+</section>
+
+<section class="secion-wrappper   mh-100vh  d-flex align-items-center justify-content-center">
+  <div class="container">
+      <div class="row text-center">
+        <div class="col-lg-8">
+        <img src="./images/alterview/av-img2.png" class="img-fluid" alt="">
+
+        </div>
+      </div>
+    
+
+  </div>
+</section>
+
+<section class="secion-wrappper bg-gray pb-0  mh-100vh ">
+  <div class="container text-center">
+
+    <div class="section-title">
+        <div class="row mb-5">
+          <div class="col-md-10 col-lg-8">
+          <h1 class="text-orange proxima font-weight-bold "><span class="text-alter-blue">OUR</span> UNIQUE OFFERINGS</h1>
+        <p class="proxima text-gray font-weight-normal  f-16 mb-3">altRview, pioneer of many new technologies and their innovative applications, has brought
+ﬁrst of its kind experiential Edutainment solutions in India.  </p>
+          </div>
+        </div>
+        
+    </div>  
+    </div>
+    <div class="offerings">
+        <div class="row no-gutters">
+          <div class="col-md-6">
+            <img src="./images/alterview/av-img3.jpg" class="w-100 h-100 object-fit-cover" alt="">
+          </div>
+          <div class="col-md-6 bg-alter-blue p-5">
+            <h3 class="proxima font-weight-bold text-orange">IMMERSIVE SPHERE (FULL)</h3>
+            <p class="proxima font-weight-normal">A 360-degree immersive view of the virtual
+environment without the need  of any external
+wearables   </p>
+          </div>
+        </div>
+        <div class="row no-gutters">
+          <div class="col-md-6 bg-alter-blue p-5">
+            <h3 class="proxima font-weight-bold text-orange">IMMERSIVE SPHERE (HALF)</h3>
+            <p class="proxima font-weight-normal">A 360-degree immersive view of the virtual
+environment without the need  of any external
+wearables   </p>
+          </div>
+          <div class="col-md-6 img-right">
+            <img src="./images/alterview/av-img4.jpg" class="w-100 h-100 object-fit-cover" alt="">
+          </div>
+        </div>
+
+        <div class="row no-gutters">
+          <div class="col-md-6">
+            <img src="./images/alterview/av-img5.jpg" class="w-100 h-100 object-fit-cover" alt="">
+          </div>
+          <div class="col-md-6 bg-alter-blue p-5">
+            <h4 class="proxima font-weight-bold text-orange">VR CAVE (CAVE AUTOMATIC VIRTUAL ENVIRONMENT) </h4>
+            <p class="proxima font-weight-normal">A room-sized 3D visualization solution to walk in to a
+simulated virtual environment</p>
+          </div>
+        </div>
+
+        <div class="row no-gutters">
+          <div class="col-md-6 bg-alter-blue p-5">
+            <h4 class="proxima font-weight-bold text-orange">AUGMENTED REALITY</h4>
+            <p class="proxima font-weight-normal">An extended perception of reality by creating a new-artiﬁcial world on top of the existing one </p>
+          </div>
+          <div class="col-md-6 img-right">
+            <img src="./images/alterview/av-img6.jpg" class="w-100 h-100 object-fit-cover" alt="">
+          </div>
+        </div>
+
+        <div class="row no-gutters">
+        <div class="col-md-6">
+            <img src="./images/alterview/av-img7.jpg" class="w-100 h-100 object-fit-cover" alt="">
+          </div>
+          <div class="col-md-6 bg-alter-blue p-5">
+            <h4 class="proxima font-weight-bold text-orange">VR BUS</h4>
+            <p class="proxima font-weight-normal">A complete immersive experience with synchronized
+motions in the seat </p>
+          </div>
+          
+        </div>
+
+        <div class="row no-gutters">
+          <div class="col-md-6 bg-alter-blue p-5">
+            <h4 class="proxima font-weight-bold text-orange">INTERACTIVE/ROBOTIC ANIMALS</h4>
+            <p class="proxima font-weight-normal">Live size animatronic animals with both interactive
+and robotic capabilities </p>
+          </div>
+          <div class="col-md-6 img-right">
+            <img src="./images/alterview/av-img8.jpg" class="w-100 h-100 object-fit-cover" alt="">
+          </div>
+        </div>
+
+        <div class="row no-gutters">
+        <div class="col-md-6">
+            <img src="./images/alterview/av-img5.jpg" class="w-100 h-100 object-fit-cover" alt="">
+          </div>
+          <div class="col-md-6 bg-alter-blue p-5">
+            <h4 class="proxima font-weight-bold text-orange">VR CAVE (CAVE AUTOMATIC VIRTUAL ENVIRONMENT) </h4>
+            <p class="proxima font-weight-normal">Live size animatronic animals with both interactive and
+robotic capabilities </p>
+          </div>
+          
+        </div>
+
+        <div class="row no-gutters">
+        
+          <div class="col-md-6 bg-alter-blue p-5">
+            <h4 class="proxima font-weight-bold text-orange">ROBOTIC HUMANS</h4>
+            <p class="proxima font-weight-normal">Live size humanoid robots with real looking faces and
+accurate motions </p>
+          </div>
+          <div class="col-md-6 img-right">
+            <img src="./images/alterview/av-img9.jpg" class="w-100 h-100 object-fit-cover" alt="">
+          </div>
+        </div>
+
+    </div>
+
+ 
+</section>
+
+<section class="secion-wrappper   mh-100vh  pb-0">
+<div class="container text-center">
+
+<div class="section-title">
+    <div class="row mb-5">
+      <div class="col-md-8">
+      <h1 class="proxima text-orange font-weight-bold "><span class="text-alter-blue">OUR</span> SOLUTIONS</h1>
+    <p class="proxima text-gray font-weight-normal  f-16 mb-3">altRview range of solutions offer a holistic learning experience and takes visitors
+on a journey of mental, emotional and intellectual exploration.  </p>
+      </div>
+    </div>
+    
+</div>  
+</div>
+
+  <div class="solutions">
+    <div class="row no-gutters">
+      <div class="col-lg-6">
+      <div class="row no-gutters solution-row h-100">
+      <div class="col-md-6  d-flex flex-column p-5">
+        <div class="flex-fill">
+        <h4 class=" proxima font-weight-bold ">LIFE LIKE ANIMATRONICS PARK </h4>
+          <p  class="proxima  font-weight-normal  f-16 mb-4">altRview uses animatronics as a tool to design,
+          develop and operate special animal/human zones using robotic and interactive models. The
+          Animatronics set-up is supported with landscape development and recreational activities,
+          corresponding to the theme to create a unique
+          theme park.   </p>
+        </div>
+          <div class="count">
+            <img src="./images/alterview/text1.png" alt="">
+          </div>
+      </div>
+
+      <div class="col-md-6  d-flex flex-column p-5">
+        <div class="fle-fill">
+        <h4 class="proxima font-weight-bold ">HIGH-END INTERPRETATION PARK</h4>
+          <p  class="proxima font-weight-normal  f-16 mb-4">Starting from identifying the goal of the park for its visitors and conceptualizing a theme that resonates with it, to selecting the right combination of interpretive methods and its implementation and development, altRview has the necessary expertise and resources to give audience the most entertaining and powerful learning experience.      </p>
+        </div>
+        <div class="count">
+          <img src="./images/alterview/text2.png" alt="">
+        </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-6 ">
+      <div class="row no-gutters solution-row row-2 h-100">
+        <div class="col-md-6 d-flex flex-column p-5">
+          <div class="flex-fill">
+          <h4 class="proxima font-weight-bold ">IMMERSIVE LEARNING DIGITAL GALLERIES </h4>
+          <p  class="proxima  font-weight-normal  f-16 mb-4">altRview is an expert in conceptualising, designing and maintaining state of art galleries. Equipped with latest technologies, our digital galleries blend science with creativity. This ignites the minds of young and old ones alike and proves to be a captivating experience. </p>
+          </div>
+        <div class="count">
+          <img src="./images/alterview/text3.png" alt="">
+        </div>
+          </div>
+
+          <div class="col-md-6  d-flex flex-column p-5">
+            <div class="flex-fill">
+            <h4 class="proxima font-weight-bold ">IMMERSIVE LEARNING EXPERIENCE</h4>
+          <p  class="proxima font-weight-normal  f-16 mb-4">altRview has all expertise of delivering an
+engaging storytelling experience delivered
+through latest technology applications to take
+visitors back in past with complete details of
+the events and characters associated with the
+monuments of historical importance.</p>
+            </div>
+         
+        <div class="count">
+          <img src="./images/alterview/text4.png" alt="">
+        </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    
+
+       
+
+      </div>
+</section>
+
+<section class="secion-wrappper   mh-100vh  d-flex align-items-center justify-content-center">
+
+  <div class="container">
+
+    <div class="row align-items-center">
+      <div class="col-md-6 mb-5 mb-md-0">
+      <div class="title-border-sm border-orange border-left pl-3 mb-4">
+          <h5 class="proxima font-weight-bold text-black  mb-2 text-alter-blue">WE BRING</h5>
+          <h3 class="text-orange proxima font-weight-bold">YOUR IDEAS TO LIFE</h3>
+        </div>
+      
+
+        <p class="proxima text-gray font-weight-normal  f-16 mb-3">No capital investments. No operational hassles. Just imagine
+what you really want to create and leave the rest on us. altRview
+functions on a <strong>Public-Private Partnership Model</strong>.</p>
+        <p class="proxima text-gray font-weight-normal  f-16 mb-3">
+We invest the necessary resources to develop, operate and
+maintain the edutainment avenues for a pre-decided time
+period, based on a mutual-agreement.</p>
+      </div>
+      <div class="col-md-6 text-center">
+        <h4 class="mb-4 proxima font-weight-bold text-alter-blue mb-4 ">End to End Solution Delivery</h4>
+        <img src="./images/alterview/av-img10.jpg"  class="w-100 e3e-img" alt="">
+      </div>
+    </div>
+
+  </div>
+</section>
+
+<?php include 'footer.php';?>
+
+
+
+</body>
+</html>
